7f59198f5e1a7279caf102a714b744196496fc7d
Ravi Sharma <ravisha@chalmers.se>
2025-12-31 00:56:35 +0100

    #1 Corroct route error and update index.css style to v4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages";
+import LandingPage from "./pages/index.tsx";
 import ResultPage from "./pages/ResultPage";
 
 const App = () => (
